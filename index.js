4ff21d19fb41f18a71fd0cfb97bcad2623376f3a
DARKCRAWLER9 <148722391+DARKCRAWLER9@users.noreply.github.com>
2023-10-23 00:55:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "thetipcoin.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 3000)
